ce3538cad80483788017425ddaeaff2e2d00476e
Treri <treri.liu@gmail.com>
2015-10-22 18:17:49 +0800

    update plugin

--- fzf-zsh.plugin.zsh
+++ fzf-zsh.plugin.zsh
@@ -17,30 +17,27 @@ fzf_path="$( cd "$fzf_zsh_path/../fzf/" && pwd )"
 export PATH="$PATH:$fzf_path/bin"
 export MANPATH="$MANPATH:$fzf_path/man"
 
-export FZF_COMPLETION_TRIGGER='~~'
-
-export FZF_DEFAULT_COMMAND='
-  (git ls-files --cached --exclude-standard --others ||
-   find * -name ".*" -prune -o -type f -print -o -type l -print) 2> /dev/null'
-
-if type ag &> /dev/null; then
-  export FZF_DEFAULT_COMMAND='(git ls-files --cached --exclude-standard --others ||
-    ag -i -l --nocolor --nogroup --hidden \
-      --ignore .git \
-      --ignore .svn \
-      --ignore .hg \
-      --ignore .DS_Store \
-      --ignore node_modules \
-      --ignore bower_components \
-      --ignore "**/*.pyc" \
-      -g "") 2> /dev/null'
-fi;
-
-# To apply the command to CTRL-T as well
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+# export FZF_DEFAULT_COMMAND='
+#   (git ls-files --cached --exclude-standard --others ||
+#    find * -name ".*" -prune -o -type f -print -o -type l -print) 2> /dev/null'
+# if type ag &> /dev/null; then
+#   export FZF_DEFAULT_COMMAND='(git ls-files --cached --exclude-standard --others ||
+#     ag -i -l --nocolor --nogroup --hidden \
+#       --ignore .git \
+#       --ignore .svn \
+#       --ignore .hg \
+#       --ignore .DS_Store \
+#       --ignore node_modules \
+#       --ignore bower_components \
+#       --ignore "**/*.pyc" \
+#       -g "") 2> /dev/null'
+# fi;
+# # To apply the command to CTRL-T as well
+# export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # auto completion is broken with zsh-autosuggestion plugin
 # so comment out this line
+# export FZF_COMPLETION_TRIGGER='~~'
 # source "$fzf_path/shell/completion.zsh"
 
 source "$fzf_path/shell/key-bindings.zsh"
